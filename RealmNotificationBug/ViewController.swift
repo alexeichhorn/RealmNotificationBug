cfb4a194f40273d04f2d93c56e413c607fd8c6a1
Alexander Eichhorn <a.eichhorn@losjet.com>
2020-12-21 00:49:17 +0100

    Initial Commit

---
+++ RealmNotificationBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmNotificationBug
+//
+//  Created by Alexander Eichhorn on 21.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
